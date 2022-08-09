7c0f79c55b0704e369a3a943dc11b60c435c9649
gids-dadi <gideons564@gmail.com>
2022-08-09 01:35:58 +0100

    Add commands for animal table exercise

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+ CREATE TABLE animals (
+ ID                              INT               PRIMARY KEY       NOT NULL,
+ NAME                       CHAR(50)                                   NOT NULL,
+ DATE_OF_BIRTH       DATE                                           NOT NULL,
+ ESCAPE_ATTEMPTS   INT                                             NOT NULL,
+ NEUTERED                BIT                                              NOT NULL,
+ WEIGHT_KG              DECIMAL
+ );
